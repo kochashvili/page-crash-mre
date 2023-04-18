6fb47a637df0a3ee0f4e2d55095d85271c80e672
Jaba Kochashvili <jaba_kochashvili@epam.com>
2023-04-18 14:43:39 +0400

    add matcher

--- src/middleware.js
+++ src/middleware.js
@@ -1,23 +1,17 @@
 import { NextResponse } from "next/server";
 
-const PUBLIC_FILE = /\.(.*)$/;
-
 export function middleware(request) {
-  const shouldHandleLocale =
-    !PUBLIC_FILE.test(request.nextUrl.pathname) &&
-    !request.nextUrl.pathname.includes("/api/");
   const url = request.nextUrl;
+  const locale = url.locale === "default" ? "en" : url.locale;
+  const pathname = decodeURI(url.pathname).toLowerCase();
 
-  if (shouldHandleLocale) {
-    const locale = url.locale === "default" ? "en" : url.locale;
-    const pathname = decodeURI(url.pathname).toLowerCase();
-
-    if (decodeURI(url.pathname) !== pathname || url.locale !== locale) {
-      return NextResponse.redirect(
-        encodeURI(`${url.origin}/${locale}${pathname}${url.search}`)
-      );
-    }
+  if (decodeURI(url.pathname) !== pathname || url.locale !== locale) {
+    return NextResponse.redirect(encodeURI(`${url.origin}/${locale}${pathname}${url.search}`));
   }
 
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
