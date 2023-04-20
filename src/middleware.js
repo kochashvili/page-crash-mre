01a0f890764bcb2f114c9b5ebe68ec8dcf2c9677
Jaba Kochashvili <jaba_kochashvili@epam.com>
2023-04-20 17:16:00 +0400

    revert to the OG matcher

--- src/middleware.js
+++ src/middleware.js
@@ -6,11 +6,23 @@ export function middleware(request) {
   const url = request.nextUrl;
   const locale = url.locale === "default" ? "en" : url.locale;
   const pathname = decodeURI(url.pathname).toLowerCase();
-  const isInternal = pathname.startsWith("/_next") || PUBLIC_FILE.test(pathname);
+  const isInternal =
+    pathname.startsWith("/_next") || PUBLIC_FILE.test(pathname);
 
-  console.log(`mdw req url: ${url}, pathname: ${pathname}, isInternal: ${isInternal}`);
+  console.log(
+    `mdw req url: ${url}, pathname: ${pathname}, isInternal: ${isInternal}`
+  );
 
-  if (!isInternal && (decodeURI(url.pathname) !== pathname || url.locale !== locale)) {
-    return NextResponse.redirect(encodeURI(`${url.origin}/${locale}${pathname}${url.search}`));
+  if (
+    !isInternal &&
+    (decodeURI(url.pathname) !== pathname || url.locale !== locale)
+  ) {
+    return NextResponse.redirect(
+      encodeURI(`${url.origin}/${locale}${pathname}${url.search}`)
+    );
   }
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
