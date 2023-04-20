6dd4c3db8fc5992c37b6bbdab143c6d34224c142
Jaba Kochashvili <jaba_kochashvili@epam.com>
2023-04-20 16:57:28 +0400

    change page route

--- src/pages/[slug]/isr.js
+++ src/pages/[slug]/isr.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../../styles/Home.module.css";
 import crypto from "crypto";
 
 export default function Home({ UUID }) {
