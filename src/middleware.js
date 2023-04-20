661380c35aa6d9c586e0f35ccd787efa6100ef26
Jaba Kochashvili <jaba_kochashvili@epam.com>
2023-04-20 17:12:35 +0400

    return undefined if not a match

--- src/middleware.js
+++ src/middleware.js
@@ -6,23 +6,11 @@ export function middleware(request) {
   const url = request.nextUrl;
   const locale = url.locale === "default" ? "en" : url.locale;
   const pathname = decodeURI(url.pathname).toLowerCase();
-  const isInternal =
-    pathname.startsWith("/_next") || PUBLIC_FILE.test(pathname);
+  const isInternal = pathname.startsWith("/_next") || PUBLIC_FILE.test(pathname);
 
-  console.log(
-    `mdw req url: ${url}, pathname: ${pathname}, it starts with _next: ${pathname.startsWith(
-      "/_next"
-    )}`
-  );
+  console.log(`mdw req url: ${url}, pathname: ${pathname}, isInternal: ${isInternal}`);
 
-  if (
-    !isInternal &&
-    (decodeURI(url.pathname) !== pathname || url.locale !== locale)
-  ) {
-    return NextResponse.redirect(
-      encodeURI(`${url.origin}/${locale}${pathname}${url.search}`)
-    );
+  if (!isInternal && (decodeURI(url.pathname) !== pathname || url.locale !== locale)) {
+    return NextResponse.redirect(encodeURI(`${url.origin}/${locale}${pathname}${url.search}`));
   }
-
-  return NextResponse.next();
 }
