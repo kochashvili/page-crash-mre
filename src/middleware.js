9543609596acd5033cdb28fd27286c3b6958b6eb
Jaba Kochashvili <jaba_kochashvili@epam.com>
2023-04-20 17:09:05 +0400

    replace logger with console

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,4 @@
 import { NextResponse } from "next/server";
-import logger from "services/logger";
 
 const PUBLIC_FILE = /\.(.*)$/;
 
@@ -7,9 +6,10 @@ export function middleware(request) {
   const url = request.nextUrl;
   const locale = url.locale === "default" ? "en" : url.locale;
   const pathname = decodeURI(url.pathname).toLowerCase();
-  const isInternal = pathname.startsWith("/_next") || PUBLIC_FILE.test(pathname)
+  const isInternal =
+    pathname.startsWith("/_next") || PUBLIC_FILE.test(pathname);
 
-  logger.info(
+  console.log(
     `mdw req url: ${url}, pathname: ${pathname}, it starts with _next: ${pathname.startsWith(
       "/_next"
     )}`
